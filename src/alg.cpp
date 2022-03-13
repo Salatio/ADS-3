32ef8c1a41b6f582fac1202458a31678c487e4c5
Salatio <99189453+Salatio@users.noreply.github.com>
2022-03-13 20:56:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0, start = 0, end = size - 1;
+	while (start < end) {
+		int mid = (start + end) / 2;
+		if (arr[mid] == value) {
+			count++;
+			int temp = mid;
+			while (arr[--mid] == value) {
+				count++;
+			}
+			while (arr[++temp] == value) {
+				count++;
+			}
+			return count;
+		} else if (arr[mid] > value) {
+			end = mid;
+		} else {
+			start = mid + 1;
+		}
+	}
+    return 0; // если ничего не найдено
 }
