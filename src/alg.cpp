011ee6f8808e7ce272c7a33eb4a8ac01edbe63dc
Salatio <99189453+Salatio@users.noreply.github.com>
2022-03-13 21:54:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,24 +2,23 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int count = 0, start = 0, end = size - 1;
-	while (start < end) {
-		int mid = (start + end) / 2;
-		if (arr[mid] == value) {
-			count++;
-			int temp = mid;
-			while (arr[--mid] == value) {
-				count++;
-			}
-			while (arr[++temp] == value) {
-				count++;
-			}
-			return count;
-		} else if (arr[mid] > value) {
-			end = mid;
-		} else {
-			start = mid + 1;
-		}
-	}
-	
+    while (start < end) {
+        int mid = (start + end) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int temp = mid;
+            while (arr[--mid] == value) {
+                count++;
+            }
+            while (arr[++temp] == value) {
+                count++;
+            }
+            return count;
+        } else if (arr[mid] > value) {
+            end = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
     return 0; // если ничего не найдено
 }
